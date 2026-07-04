9efcbbaa69a7812d499b8ac0f0863d33f2a57713
ahmedabdishukri309-ai <ahmedabdishukri309@gmail.com>
2026-07-04 04:12:36 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,53 @@
 lib/main.dart
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const KickoraApp());
+}
+
+class KickoraApp extends StatelessWidget {
+  const KickoraApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kickora',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.green,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF121212),
+      appBar: AppBar(
+        title: const Text("Kickora"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: const Center(
+        child: Text(
+          "⚽ Welcome to Kickora!\nEvery Match. Every Goal. Instantly.",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 22,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
